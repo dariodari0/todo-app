e5f1eb86512d07a96a09e2f4bc26435b8199da6e
Dariusz Markowicz <dmarkowicz@outlook.com>
2018-01-16 19:28:10 +0100

    Update index.js

--- api/models/index.js
+++ api/models/index.js
@@ -1,1 +1,5 @@
+var mongoose = require('mongoose'); 
+mongoose.set('debug', true); // to set debug mode (allow to see what happening wrong)
+mongoose.connect('mongodb://localhost/api'); // connect to aplication database
 
+mongoose.Promise = Promise; //for using Promise syntax (then)
