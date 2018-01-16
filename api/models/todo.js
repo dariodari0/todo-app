2582123e23effc537080aba093624310ba43425c
Dariusz Markowicz <dmarkowicz@outlook.com>
2018-01-16 21:47:21 +0100

    Update todo.js

--- api/models/todo.js
+++ api/models/todo.js
@@ -1,1 +1,21 @@
+//Schema: name, completed, created_date
 
+var mongoose = require("mongoose");
+
+var toDoSchema =  new mongoose.Schema({
+    name: {
+    type: String,
+    required: 'Name cannot be empty!'
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var toDo = mongoose.model('toDo', toDoSchema );
+module.exports = toDo;
