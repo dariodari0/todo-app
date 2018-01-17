18272241bc8842b761a155de09041f78f7b3983d
Dariusz Markowicz <dmarkowicz@outlook.com>
2018-01-17 23:29:00 +0100

    Update todos.js

--- api/routes/todos.js
+++ api/routes/todos.js
@@ -1,8 +1,15 @@
 var express =  require('express');
 var router = express.Router();
+var db = require("../models"); //make connet and take database variables
 
 router.get('/', function(req,res){
-    res.send("helllo from todos routes");
-})
+    db.toDo.find()
+    .then(function(todos){
+     res.json(todos);
+    })
+    .catch(function(err){
+      res.send(err);
+    })
+});
 
 module.exports = router;
