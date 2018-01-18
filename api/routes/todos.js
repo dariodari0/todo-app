596aa0b7b3295e4800ebc01fcd236b40252d5454
Dariusz Markowicz <dmarkowicz@outlook.com>
2018-01-18 17:32:12 +0100

    refactoring code and import from helpers

--- api/routes/todos.js
+++ api/routes/todos.js
@@ -1,59 +1,15 @@
 var express =  require('express');
 var router = express.Router();
 var db = require("../models"); //make connect and take database variables
+var helpers = require("../helpers/todos");
 
-//list all todos GET route  - defining the Index Route - GET
-router.get('/', function(req,res){
-    db.toDo.find()
-    .then(function(todos){
-     res.json(todos);
-    })
-    .catch(function(err){
-      res.send(err);
-    })
-});
-// Create new todo POST Route - defining the Create Route - POST
-router.post('/', function(req,res){
-    db.toDo.create(req.body)
-    .then(function(newToDo){
-        res.status(201).json(newToDo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
-
-// Retrieve a todo - defining the Show Route - GET
-router.get('/:todoId', function(req,res){
-    db.toDo.findById(req.params.todoId)
-    .then(function(foundTodo){
-        res.json(foundTodo);
-    })
-    .catch(function(err){
-        res.send(err);
-    })
-});
-
-// Update a todo - defining the Update Route - PUT
-router.put('/:todoId', function(req,res){
-   db.toDo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
-   .then(function(todo){
-       res.json(todo);
-   })
-   .catch(function(err){
-       res.send(err);
-   })
-});
-
-// Delete a todo - defining Delete Route - DELETE
-router.delete('/:todoId', function(req,res){
-   db.toDo.remove({_id: req.params.todoId})
-   .then(function(){
-       res.send({message: "Now, it is removed!"});
-   })
-   .catch(function(err){
-       res.send(err);
-   })
-});
+router.route('/')
+    .get(helpers.getTodos)
+    .post(helpers.createTodo)
+    
+router.route('/:todoId')
+    .get(helpers.getTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo)
 
 module.exports = router;
