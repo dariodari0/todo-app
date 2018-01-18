37b6cdf8f3d1d5e2621356750e74cf41ca5d9864
Dariusz Markowicz <dmarkowicz@outlook.com>
2018-01-18 22:22:34 +0100

    connect with index.html

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000,
-    ip = process.env.IP,
     bodyParser = require('body-parser');
-    
     
 var toDoRoutes = require('./routes/todos');
 
-
 app.use(bodyParser.json()); // to handle that data coming in Express doesnt give access to it automatically
 app.use(bodyParser.urlencoded({extended: true}));// this two lines: allow us to access the req.body that commes in a POST or a PUT req
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views')); // referencing directory
     
 app.get('/', function(req,res){
-        res.send("hello from the root route");
+        res.sendFile("index.html");
 });
  
 app.use('/api/todos', toDoRoutes); 
- 
-app.listen(port, function(){
-    console.log("APP IS RUNNNING! on PORT: "+port+", and IP is: " +ip);
-})
