68eaa1bfc53a3e4aaf9680a9bfb14215da317aa6
Dariusz Markowicz <dmarkowicz@outlook.com>
2018-01-18 17:06:15 +0100

    add body-parser to handle data

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    ip = process.env.IP,
+    bodyParser = require('body-parser');
+    
     
 var toDoRoutes = require('./routes/todos');
+
+
+app.use(bodyParser.json()); // to handle that data coming in Express doesnt give access to it automatically
+app.use(bodyParser.urlencoded({extended: true}));// this two lines: allow us to access the req.body that commes in a POST or a PUT req
     
 app.get('/', function(req,res){
         res.send("hello from the root route");
 });
  
-app.use('/api/todos', toDoRoutes); // add and use route from routes directory
+app.use('/api/todos', toDoRoutes); 
  
 app.listen(port, function(){
-    console.log("APP IS RUNNNING! on PORT: "+port);
+    console.log("APP IS RUNNNING! on PORT: "+port+", and IP is: " +ip);
 })
